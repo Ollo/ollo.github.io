bde4b9d232c64ef610143047235d1a5d3c2617f3
Ollo <joshua@ollomedia.com>
2013-06-04 12:24:05 -0500

    start to resume

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<head>
+    <title></title>
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <link href="css/main.css" rel="stylesheet" type="text/css" />
+    
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    
+    <!--[if lt IE 9]>
+    <script src="js/vendor/html5shiv.js"></script>
+    <![endif]-->
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>!window.jQuery && document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    
+    <!--[if (gte IE 6)&(lte IE 8)]>
+        <script type="text/javascript" src="js/vendor/selectivizr-min.js"></script>
+        <noscript><link rel="stylesheet" href="/ie.css" /></noscript>
+    <![endif]-->
+</head>
+
+<!--[if lt IE 7 ]>  <body class="ie6"> <![endif]-->
+<!--[if IE 7 ]>     <body class="ie7"> <![endif]-->
+<!--[if IE 8 ]>     <body class="ie8"> <![endif]-->
+<!--[if IE 9 ]>     <body class="ie9"> <![endif<]-->
+<!--[if (gt IE 9)|!(IE)]><!-->  <!--<![endif]-->
+
+    <header id="intro">
+
+
+    </header>
+    <div class="wrap">
+        <section>
+            <article>
+            </article>
+        </section>
+        <aside class="sidebar">
+
+        </aside>
+    </div>
+    <footer>
+
+    </footer>
+    <script type="text/javascript" src="js/main.js"></script>
+
+</body>
+</html>
